8bfe4553b8ba144c19cf219d072126db8f6d1507
Ana Silva <ananunesdasilva@gmail.com>
2019-09-01 19:37:17 +0100

    Add setCities action and cities reducer

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,14 +1,34 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import City from '../containers/city';
+import setCities from '../actions';
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
 
 class CityList extends Component {
+  componentWillMount() {
+    this.props.setCities();
+  }
+
   render() {
     return (
       <div className="cities">
         {this.props.cities.map(city => <City name={city.name} key={city.name} />)}
       </div>
-    )
+    );
   }
 }
 
-export default CityList;
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
