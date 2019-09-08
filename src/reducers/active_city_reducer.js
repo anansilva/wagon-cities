c0e8cdd3f6f6148a648adc95cee74e2cc20fe00c
Ana Silva <ananunesdasilva@gmail.com>
2019-09-08 16:59:21 +0100

    Add default null in action state

--- src/reducers/active_city_reducer.js
+++ src/reducers/active_city_reducer.js
@@ -1,8 +1,4 @@
 const activeCityReducer = (state = null, action) => {
-  if (state === undefined) {
-    return [];
-  }
-
   switch (action.type) {
     case 'SET_ACTIVE_CITY':
       return action.payload;
--- src/reducers/cities_reducer.js
+++ src/reducers/cities_reducer.js
@@ -1,10 +1,6 @@
 import cities from '../../data/cities';
 
 const citiesReducer = (state = null, action) => {
-  if (state === undefined) {
-    return [];
-  }
-
   switch (action.type) {
     case 'SET_ACTIVE_CITY':
       return state;
